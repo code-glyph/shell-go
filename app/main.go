fed1cfd2b0410bbd9bad20aec95f4acc58c48326
code-glyph <durvabdev@outlook.com>
2026-05-23 17:10:14 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"os"
+	"fmt"
+	"bufio"
 )
 
 func main() {
 	_, _ = os.Stdout.WriteString("$ ")
+	var cmd string
+	var err error
+	cmd, err = bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading command:", err)
+		return
+	}
+	cmd = cmd[:len(cmd)-1] // Remove the newline character
+	fmt.Printf("%s: command not found\n", cmd)
 }
