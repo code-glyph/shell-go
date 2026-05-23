9600f21ff091b74ddae5817256f5962837616cfe
code-glyph <durvabdev@outlook.com>
2026-05-23 17:11:44 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,11 +10,14 @@ func main() {
 	_, _ = os.Stdout.WriteString("$ ")
 	var cmd string
 	var err error
-	cmd, err = bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading command:", err)
-		return
+	var reader := bufio.NewReader(os.Stdin)
+	for {
+		cmd, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading command:", err)
+			return
+		}
+		cmd = cmd[:len(cmd)-1] // Remove the newline character
+		fmt.Printf("%s: command not found\n", cmd)
 	}
-	cmd = cmd[:len(cmd)-1] // Remove the newline character
-	fmt.Printf("%s: command not found\n", cmd)
 }
